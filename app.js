0f1019117f4804c4a87c392f8d94433a4345d292
shivG1zmo <2024103026@student.annauniv.edu>
2026-04-23 20:53:07 +0530

    Add Pomodoro work-break cycle

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let reset = document.getElementById("reset")
 let timeLeft = 1500
 let timer = null
 let isRunning = false
+let mode = "work"
 
 function convert() {
     let min = Math.floor(timeLeft/60)
@@ -19,3 +20,73 @@ function updateDisplay() {
     let display = convert()
     timerdisp.textContent = display   
 }
+
+start.addEventListener('click',()=> {
+
+    if(isRunning === true) {
+        alert("Timer already running")
+        return
+    }
+    else {
+        isRunning = true 
+        timer = setInterval(()=> {
+            --timeLeft
+            updateDisplay()
+            if(timeLeft===0){
+                if(mode === "work"){
+                alert('Break time 5 minutes')
+                mode = "break"
+                clearInterval(timer)
+                isRunning = false
+                timer = null 
+                timeLeft = 300   
+                updateDisplay()
+                }
+                else if(mode === "break"){
+                    alert('Back to work')
+                    mode = "work"
+                    clearInterval(timer)
+                    isRunning = false
+                    timer = null
+                    timeLeft = 1500
+                    updateDisplay()
+                }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+            }
+        },1000)
+    }
+
+})
+
+pause.addEventListener('click', ()=> {
+    if(isRunning === false && timeLeft!== 1500) {
+        alert('Already paused')
+        return 
+    }
+    else if(isRunning === false && timeLeft === 1500){
+        alert('Timer not started yet')
+    }
+    else {
+        clearInterval(timer)
+        isRunning = false
+        timer = null
+    }
+})
+
+reset.addEventListener('click', ()=> {
+    if(timeLeft === 1500){
+        alert('Timer already reset')
+    }   
+    else {
+    if(mode==="break"){
+        alert('Back to work')
+        mode = "work"
+    } 
+    clearInterval(timer)
+    isRunning = false
+    timer = null
+    timeLeft = 1500
+    updateDisplay()
+}})
+
+
+updateDisplay()
